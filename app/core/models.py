7a68b3656829ea8c0190c945e972d131a9b8f5b2
Aziz Marzouki <aziz@authentise.com>
2023-05-14 23:15:53 +0100

    customized user admin

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,45 @@
-from django.db import models # noqa
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and return new super-user"""
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """System user"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.EmailField(max_length=255)
+    is_active = models.BooleanField(
+        default=True,
+        help_text="Designates whether this user should be treated as active. "
+                  "Unselect this instead of deleting accounts.",
+    )
+    is_staff = models.BooleanField(
+        default=False,
+        help_text="Designates whether the user " "can log into this admin site.",
+    )
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
